6aeeb2c9f5b0e2bea6d0ea989dc33a40038ba51f
UP2017778 <sking233952@gmail.com>
2023-03-07 20:54:28 +0000

    feature(Swiper): Added pan animation for swiping, also touched up on the cards being modular

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,37 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useRef } from 'react';
+import { View, Text, StyleSheet, Animated, PanResponder } from 'react-native';
 import { Icon } from 'react-native-elements'
 
 import { theme } from '../theme'
 
-export default function Card({ children }) {
+export default function Card({ children, style, name, age }) {
+    const pan = useRef(new Animated.ValueXY()).current;
+
+    const panResponder = useRef(
+        PanResponder.create({
+            onMoveShouldSetPanResponder: () => true,
+            onPanResponderMove: Animated.event(
+                [null, { dx: pan.x, dy: pan.y }],
+                { useNativeDriver: false }
+            ),
+            onPanResponderRelease: () => {
+                Animated.spring(pan, {
+                    toValue: { x: 0, y: 0 },
+                    useNativeDriver: false,
+                }).start();
+            },
+        })
+    ).current;
+    
     return (
-        <View style={styles.container}>
-            <Text style={styles.heading}>Jane Doe</Text>
+        <Animated.View
+            style={[pan.getLayout(), styles.container, style]}
+            {...panResponder.panHandlers}
+        >
+            <View style={styles.person}>
+                <Text style={styles.heading}>{name}</Text>
+                <Text style={styles.heading}>{age}</Text>
+            </View>
             <Text style={styles.text}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</Text>
             <View style={styles.children}>{children}</View>
             <View style={styles.row}>
@@ -25,13 +49,13 @@ export default function Card({ children }) {
                     size={50}
                 />
             </View>
-        </View>
+        </Animated.View>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-        width: '80%',
+        width: '90%',
         height: '70%',
         padding: 15,
 
@@ -39,6 +63,7 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         backgroundColor: theme.colors.accent,
         borderRadius: 20,
+
     },
     row: {
         width: '75%',
@@ -52,12 +77,20 @@ const styles = StyleSheet.create({
         justifyContent: 'space-between',
         alignItems: 'center',
     },
+    person: {
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: 10,
+    },
     heading: {
         fontSize: 24,
         fontWeight: 'bold',
     },
     text: {
-        fontSize: 18,
+        width: '75%',
+        fontSize: 14,
         textAlign: 'center',
     },
     children: {
@@ -66,5 +99,5 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         justifyContent: 'space-between',
         alignItems: 'center',
-    },
+    }
 });
--- src/screens/MatchesScreen.js
+++ src/screens/MatchesScreen.js
@@ -15,7 +15,40 @@ export default function MatchesScreen({ navigation }) {
         <Background>
             <Header/>
 
-            <Card>
+            <Card
+                name='John Doe'
+                age='20'
+                style={{
+                    zIndex: 2,
+                    position: 'relative',
+                    backgroundColor: '#FFBF00',
+                }}
+            >
+                <CardItem>
+                    <Text>Swimming</Text>
+                </CardItem>
+                <CardItem>
+                    <Text>Scorpio</Text>
+                </CardItem>
+                <CardItem>
+                    <Text>Drinks</Text>
+                </CardItem>
+                <CardItem>
+                    <Text>Shopping</Text>
+                </CardItem>
+            </Card>
+
+            <Card
+                name='Jane Doe'
+                age='25'
+                style={{
+                    zIndex: 1,
+                    position: 'absolute',
+                    top: 126.5,
+                    left: 19.5,
+                    backgroundColor: '#F28C28',
+                }}
+            >
                 <CardItem>
                     <Text>Swimming</Text>
                 </CardItem>
